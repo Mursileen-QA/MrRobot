37d79812ad8b0baffab3760efc7fedd191ec5b41
Admin <mursyleen@gmail.com>
2020-10-25 23:09:28 +0530

    MASTER FIRST COMMIT

---
+++ src/ClassA.java
@@ -0,0 +1,16 @@
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void A()
+	{
+		
+		System.out.println("A");
+		
+	}
+	
+}
